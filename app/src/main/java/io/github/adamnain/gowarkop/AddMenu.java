6aeedc10d51a12b51fc70247c5b4cddd391656a6
Aditya123666 <Adityanugrahaadha9@gmail.com>
2018-05-28 18:22:42 +0700

    Edit

--- app/src/main/java/io/github/adamnain/gowarkop/AddMenu.java
+++ app/src/main/java/io/github/adamnain/gowarkop/AddMenu.java
@@ -2,12 +2,22 @@ package io.github.adamnain.gowarkop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddMenu extends AppCompatActivity {
+
+    Button addmenu;
+    EditText etnamamakanan,hargamakanan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_menu);
+
+        addmenu = (Button) findViewById(R.id.btn_addmenu);
+        etnamamakanan = (EditText) findViewById(R.id.et_namamakanan);
+        hargamakanan = (EditText) findViewById(R.id.et_hargamakanan);
     }
 }
